95583fb6dccd6a5a458fd74d3c7ee9acfe46d089
Jenny Martin <jennymartin@Jennys-MBP.lan1>
2019-05-21 23:37:38 -0500

    first commit,need to update cast code

---
+++ controllers/api/movies.js
@@ -0,0 +1,48 @@
+var Movie = require('../../models/movie');
+var Performer = require('../../models/performer');
+
+module.exports = {
+  getAllMovies,
+  getOneMovie,
+  createMovie,
+  updateMovie,
+  deleteMovie
+};
+
+function deleteMovie(req, res) {
+    Movie.findByIdAndDelete(req.params.id)
+    .then(function(movie){
+        res.status(200).json(movie);
+    });
+}
+
+function updateMovie(req, res) {
+    Movie.findByIdAndUpdate(req.params.id, req.body, {new: true})
+    .then(function(movie){
+        res.status(200).json(movie);
+    });
+}
+
+function createMovie(req, res) {
+    Movie.create(req.body)
+    .then(function(movie) {
+        res.status(201).json(movie);
+    });
+  }
+
+function getOneMovie(req, res) {
+    Movie.findById(req.params.id)
+    .populate('cast')
+    .then(function(movie) {
+    res.status(200).json(movie);
+      });
+    }
+    
+
+function getAllMovies(req, res) {
+  Movie.find({})
+  .populate('cast')
+  .then(function(movies) {
+    res.status(200).json(movies);
+});
+}
